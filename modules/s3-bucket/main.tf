e9f4e2bafe0be39aed22c4408522e6a0f6e1b5b8
Mike Roberts <mike70@gmail.com>
2022-08-10 11:56:30 -0400

    update s3 configuration for new TF S3 ACL and versioning spec

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -1,9 +1,16 @@
 resource "aws_s3_bucket" "data_bucket" {
-  acl = var.bucket_acl
-  
-  versioning {
-    enabled = var.versioning_enabled
-  }
-
   tags = var.resource_tags
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.data_bucket.id
+  acl    = var.bucket_acl
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.data_bucket.id
+  
+  versioning_configuration {
+    status = var.versioning_enabled
+  }
 }
