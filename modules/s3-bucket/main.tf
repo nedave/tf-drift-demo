e6d804d55300179abba57fd8daa287d75fe5cf72
Mike Roberts <mike70@gmail.com>
2022-08-10 12:31:37 -0400

    fix s3 bucket names in main.tf

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -2,12 +2,12 @@ resource "aws_s3_bucket" "data_bucket" {
   tags = var.resource_tags
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "data_bucket_acl" {
   bucket = aws_s3_bucket.data_bucket.id
   acl    = var.bucket_acl
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
+resource "aws_s3_bucket_versioning" "data_bucket_versioning" {
   bucket = aws_s3_bucket.data_bucket.id
   
   versioning_configuration {
