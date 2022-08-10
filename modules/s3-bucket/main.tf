a33e0f3cbebc8f26d03e11492dd78895504a912a
Mike Roberts <mike70@gmail.com>
2022-08-10 12:47:42 -0400

    change acl and versioning resource to match data bucket

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -2,12 +2,12 @@ resource "aws_s3_bucket" "data_bucket" {
   tags = var.resource_tags
 }
 
-resource "aws_s3_bucket_acl" "data_bucket_acl" {
+resource "aws_s3_bucket_acl" "data_bucket" {
   bucket = aws_s3_bucket.data_bucket.id
   acl    = var.bucket_acl
 }
 
-resource "aws_s3_bucket_versioning" "data_bucket_versioning" {
+resource "aws_s3_bucket_versioning" "data_bucket" {
   bucket = aws_s3_bucket.data_bucket.id
   
   versioning_configuration {
